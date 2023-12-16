2b48fa7d9de92aa9b0f1351afc3e1fa94a5920e7
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-12-16 18:19:56 +0530

    Update SERVER_URL in getServerUrl function

--- app/util/functions.ts
+++ app/util/functions.ts
@@ -1,7 +1,7 @@
 export function getServerUrl(url: string) {
 	let SERVER_URL;
 	if (process.env.NODE_ENV === "production") {
-		SERVER_URL = "https://supercell.vercel.app";
+		SERVER_URL = "https://clash-of-codes-api.onrender.com/";
 	} else {
 		SERVER_URL = "http://localhost:3001";
 	}
