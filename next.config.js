b0929807af3079a3d9c096eb954a41c282674ac2
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-12-16 19:41:40 +0530

    Update Cross-Origin-Opener-Policy value to "same-origin-allow-popups"

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 				headers: [
 					{
 						key: "Cross-Origin-Opener-Policy",
-						value: "same-origin", // "same-origin-allow-popups"
+						value: "same-origin-allow-popups", // "same-origin-allow-popups"
 					},
 				],
 			},
