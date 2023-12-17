93f833f198c747b9b960097d965690f120abbea4
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-12-17 21:44:15 +0530

    Refactor ConfigItem component

--- app/components/ConfigItem/ConfigItem.tsx
+++ app/components/ConfigItem/ConfigItem.tsx
@@ -1,8 +1,8 @@
+import Trash from "@/app/styles/Icons/Trash";
 import { Contest } from "@/app/util/types";
 import { DeleteIcon } from "@chakra-ui/icons";
 import { IconButton, Switch } from "@chakra-ui/react";
 import styles from "./ConfigItem.module.css";
-import Trash from "@/app/styles/Icons/Trash";
 
 function fullForm(short: string) {
   switch (short) {
@@ -20,50 +20,34 @@ function fullForm(short: string) {
 }
 
 export default function ConfigItem({ itemData }: { itemData: Contest }) {
-	console.log("item data", itemData);
-	return (
-		<div className={styles.main}>
-			{/* Order:
-  console.log("item data", itemData);
-
-  const Team1 = fullForm(itemData.Team1);
-  const Team2 = fullForm(itemData.Team2);
+  const team1 = fullForm(itemData.Team1);
+  const team2 = fullForm(itemData.Team2);
 
   return (
     <div className={styles.main}>
-      <div>{Team1}</div>
-      {/* Order:
-				-Team1
-				-Team2
-				-Contest Code
-				-Date Added
-				-isLive Toggle
-				-Remove Button
-			*/}
+      <div>{team1}</div>
 
-			<div>{itemData.Team1}</div>
+      <div>{team2}</div>
 
-			<div>{itemData.Team2}</div>
+      <div>{itemData.ContestCode}</div>
 
-			<div>{itemData.ContestCode}</div>
-
-			<div>{itemData.DateAdded}</div>
+      <div>{itemData.DateAdded}</div>
 
       <div>
         <Switch variant={"default"} size="lg" defaultChecked={itemData.Live} />
       </div>
 
-			<div>
-				<IconButton
-					isRound={true}
-					variant=""
-					size={"lg"}
-					aria-label="Done"
-					fontSize="20px"
-					color="red.600"
-					icon={<Trash />}
-				/>
-			</div>
-		</div>
-	);
+      <div>
+        <IconButton
+          isRound={true}
+          variant=""
+          size={"lg"}
+          aria-label="Done"
+          fontSize="20px"
+          color="red.600"
+          icon={<Trash />}
+        />
+      </div>
+    </div>
+  );
 }
