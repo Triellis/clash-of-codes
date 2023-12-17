2318d2868287c489bd128c611af3a4fcf9425980
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-12-17 19:42:38 +0530

    Refactor Config module and ConfigItem component

--- app/components/ConfigItem/ConfigItem.tsx
+++ app/components/ConfigItem/ConfigItem.tsx
@@ -1,5 +1,30 @@
 import { Contest } from "@/app/util/types";
+import styles from "./ConfigItem.module.css";
 
 export default function ConfigItem({ itemData }: { itemData: Contest }) {
-	return <div>configItem</div>;
+  console.log("item data", itemData);
+  return (
+    <div className={styles.main}>
+      {/* Order:
+				-Team1
+				-Team2
+				-Contest Code
+				-Date Added
+				-isLive Toggle
+				-Remove Button
+			*/}
+
+      <div>Team1</div>
+
+      <div>Team2</div>
+
+      <div>Contest Code</div>
+
+      <div>Date Added</div>
+
+      <div>isLive Toggle</div>
+
+      <div>Remove Button</div>
+    </div>
+  );
 }
