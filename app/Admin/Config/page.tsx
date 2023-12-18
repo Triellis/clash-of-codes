30e36c5d1f37e97c4f020d8e0ed9bb6037ea73b4
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-12-18 20:30:55 +0530

    Refactor Config component

--- app/Admin/Config/page.tsx
+++ app/Admin/Config/page.tsx
@@ -7,138 +7,129 @@ import { fetcher, getServerUrl } from "@/app/util/functions";
 import { ContestCol } from "@/app/util/types";
 import { AddIcon } from "@chakra-ui/icons";
 import {
-	Button,
-	Divider,
-	Flex,
-	Heading,
-	IconButton,
-	Input,
-	Select,
-	Switch,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  Heading,
+  IconButton,
+  Input,
+  Select,
+  Switch,
 } from "@chakra-ui/react";
 import useSWR from "swr";
 import styles from "./Config.module.css";
 
 type Option = {
-	value: string;
-	label: string;
+  value: string;
+  label: string;
 };
 
 type CustomSelectProps = {
-	selectOptions: Option[];
-	teamName: string;
+  selectOptions: Option[];
+  teamName: string;
 };
 
 function CustomSelect({ selectOptions, teamName }: CustomSelectProps) {
-	return (
-		<Select variant="default" placeholder={`Select ${teamName}`} size="sm">
-			{selectOptions?.map((option) => (
-				<option key={option.value} value={option.value}>
-					{option.label}
-				</option>
-			))}
-		</Select>
-	);
+  return (
+    <Select variant="default" placeholder={`Select ${teamName}`} size="sm">
+      {selectOptions?.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </Select>
+  );
 }
 
 function useConfig(page: number) {
-	const { data, error, isLoading, mutate } = useSWR(
-		getServerUrl(`admin/config?page=${page}&maxResults=5`),
-		fetcher
-	);
+  const { data, error, isLoading, mutate } = useSWR(
+    getServerUrl(`admin/config?page=${page}&maxResults=5`),
+    fetcher
+  );
 
-	return {
-		contests: data as ContestCol[],
-		isLoading,
-		isError: error,
-		mutate,
-	};
+  return {
+    contests: data as ContestCol[],
+    isLoading,
+    isError: error,
+    mutate,
+  };
 }
+
 export default function Config() {
-	const { contests, isLoading, isError, mutate } = useConfig(1);
+  const { contests, isLoading, isError, mutate } = useConfig(1);
 
-	let contestNodes;
+  let contestNodes;
 
-	if (isLoading) contestNodes = <div>Loading...</div>;
-	if (isError) contestNodes = <div>Error...</div>;
-	if (contests) {
-		contestNodes = contests?.map((contest) => {
-			return (
-				<ConfigItem
-					key={String(contest._id!)}
-					itemData={contest as ContestCol}
-				/>
-			);
-		});
-	}
+  if (isLoading) contestNodes = <Center>Loading...</Center>;
+  if (isError) contestNodes = <Center>Error...</Center>;
+  if (contests) {
+    contestNodes = contests?.map((contest) => {
+      return (
+        <ConfigItem
+          key={String(contest._id!)}
+          itemData={contest as ContestCol}
+        />
+      );
+    });
+  }
 
-	const selectOptions = [
-		{ value: "BW", label: "Blue Wizards" },
-		{ value: "YB", label: "Yellow Barbarians" },
-		{ value: "RG", label: "Red Giants" },
-		{ value: "PP", label: "Purple Pekkas" },
-	];
+  const selectOptions = [
+    { value: "BW", label: "Blue Wizards" },
+    { value: "YB", label: "Yellow Barbarians" },
+    { value: "RG", label: "Red Giants" },
+    { value: "PP", label: "Purple Pekkas" },
+  ];
 
-	return (
-		<main className={styles.config}>
-			<Heading fontSize={"32px"} marginBlock={"64px"}>
-				Active Contests
-			</Heading>
+  return (
+    <main className={styles.config}>
+      <Heading fontSize={"32px"} marginBlock={"64px"}>
+        Active Contests
+      </Heading>
 
-			{/* form for makign the item: */}
+      {/* form for makign the item: */}
 
-			<div className={styles.configBoard}>
-				<div className={styles.header}>
-					<SpecialTxt>Team1</SpecialTxt>
-					<SpecialTxt>Team2</SpecialTxt>
-					<SpecialTxt>Contest Code</SpecialTxt>
-					<SpecialTxt>Date </SpecialTxt>
-					<SpecialTxt>Live </SpecialTxt>
-					<SpecialTxt>Remove</SpecialTxt>
-				</div>
+      <div className={styles.configBoard}>
+        <div className={styles.header}>
+          <SpecialTxt>Team1</SpecialTxt>
+          <SpecialTxt>Team2</SpecialTxt>
+          <SpecialTxt>Contest Code</SpecialTxt>
+          <SpecialTxt>Date </SpecialTxt>
+          <SpecialTxt>Live </SpecialTxt>
+          <SpecialTxt>Remove</SpecialTxt>
+        </div>
 
-				<Divider />
+        <Divider />
 
-				<div className={styles.configForm}>
-					{/* team1 */}
-					<CustomSelect
-						selectOptions={selectOptions}
-						teamName="Team 1"
-					/>
+        <div className={styles.configForm}>
+          {/* team1 */}
+          <CustomSelect selectOptions={selectOptions} teamName="Team 1" />
 
-					{/* team2 */}
-					<CustomSelect
-						selectOptions={selectOptions}
-						teamName="Team 2"
-					/>
+          {/* team2 */}
+          <CustomSelect selectOptions={selectOptions} teamName="Team 2" />
 
-					{/* contest code */}
-					<Input variant="default" placeholder="Contest Code" />
+          {/* contest code */}
+          <Input variant="default" placeholder="Contest Code" />
 
-					{/* date */}
-					<div>Today</div>
+          {/* date */}
+          <div>Today</div>
 
-					{/* isLive */}
-					<Switch
-						variant={"default"}
-						size="lg"
-						disabled
-						defaultChecked
-					/>
+          {/* isLive */}
+          <Switch variant={"default"} size="lg" disabled defaultChecked />
 
-					{/* add button */}
-					<IconButton
-						aria-label="Add"
-						icon={<AddIcon />}
-						width={"64px"}
-						height={"48px"}
-						borderRadius={"16px"}
-					/>
-				</div>
-				{contestNodes}
-			</div>
+          {/* add button */}
+          <IconButton
+            aria-label="Add"
+            icon={<AddIcon />}
+            width={"64px"}
+            height={"48px"}
+            borderRadius={"16px"}
+          />
+        </div>
+        {contestNodes}
+      </div>
 
-			<Pagination />
-		</main>
-	);
+      <Pagination />
+    </main>
+  );
 }
