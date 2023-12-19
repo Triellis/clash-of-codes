9767dea30462efe1f6902fd9e2bfc082d016f568
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-12-19 10:44:24 +0530

    Update NavBar styling and layout as per figma

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,33 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./styles/globals.css";
-import { Providers } from "./providers";
-import NavBar from "./components/NavBar/NavBar";
 import { useState } from "react";
+import NavBar from "./components/NavBar/NavBar";
+import { Providers } from "./providers";
+import "./styles/globals.css";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Clash Of Codes",
-	description: "The Live Coding Competition",
+  title: "Clash Of Codes",
+  description: "The Live Coding Competition",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	// const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-	return (
-		<html lang="en">
-			<body>
-				<Providers>
-					<div id="root">
-						<NavBar />
-						{children}
-					</div>
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          <div className="nav">
+            <NavBar />
+          </div>
+          <div id="root">{children}</div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
