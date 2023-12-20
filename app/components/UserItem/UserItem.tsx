7cc5b61c47ff603a354b57b3529717f911bd6fdd
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-12-20 17:08:34 +0530

    Add input fields and custom selects for user details

--- app/components/UserItem/UserItem.tsx
+++ app/components/UserItem/UserItem.tsx
@@ -3,26 +3,67 @@ import Edit from "@/app/styles/Icons/Edit";
 import Tick from "@/app/styles/Icons/Tick";
 import Trash from "@/app/styles/Icons/Trash";
 import { CheckIcon } from "@chakra-ui/icons";
-import { IconButton } from "@chakra-ui/react";
-import { useState } from "react";
+import { IconButton, Input } from "@chakra-ui/react";
+import { useMemo, useState } from "react";
+import CustomSelect from "../CustomSelect/CustomSelect";
 import styles from "./UserItem.module.css";
 
 function UserItem() {
   const [editMode, setEditMode] = useState(false);
+  const selectOptions = useMemo(
+    () => [
+      { value: "admin", label: "admin" },
+      { value: "leader", label: "leader" },
+      { value: "co-leader", label: "co-leader" },
+      { value: "elder", label: "elder" },
+      { value: "member", label: "member" },
+      { value: "guest", label: "guest" },
+    ],
+    []
+  );
+
+  const selectClan = useMemo(
+    () => [
+      { value: "BW", label: "Blue Wizards" },
+      { value: "YB", label: "Yellow Barbarians" },
+      { value: "RG", label: "Red Giants" },
+      { value: "PP", label: "Purple Pekkas" },
+    ],
+    []
+  );
 
   if (editMode)
     return (
       <div className={styles.main}>
         <div className={styles.first}>
-          <div>Name</div>
-          <div>Email</div>
+          <div>
+            <Input variant={"default"} placeholder="Name" size="sm" />
+          </div>
+          <div>
+            <Input variant={"default"} placeholder="Email" size="sm" />
+          </div>
         </div>
 
-        <div>UserName</div>
+        <div>
+          <Input variant={"default"} placeholder="Username" size="sm" />
+        </div>
 
-        <div>Status</div>
+        <div>
+          <CustomSelect
+            selectOptions={selectOptions}
+            option={"member"}
+            setOption={() => {}}
+          />
+        </div>
 
-        <div>Clan</div>
+        <div>
+          {" "}
+          <CustomSelect
+            selectOptions={selectClan}
+            option={"BW"}
+            setOption={() => {}}
+          />
+        </div>
 
         <div className={styles.last}>
           <div>
