1535085fa0d7ab13a7c4e3ee9ffb0f4f5b4fca43
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-12-20 17:20:55 +0530

    Update Close and Tick icons

--- app/styles/Icons/Tick.tsx
+++ app/styles/Icons/Tick.tsx
@@ -5,14 +5,14 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={24}
-    height={24}
+    height={20}
     fill="none"
     {...props}
   >
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M19.994 5.436a.75.75 0 0 1 .07 1.058l-10.5 12a.75.75 0 0 1-1.094.036l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.933 3.933 9.973-11.397a.75.75 0 0 1 1.058-.07Z"
+      d="M23.627.452c.454.397.5 1.086.103 1.54L8.457 19.445a1.09 1.09 0 0 1-1.592.053L.32 12.953a1.09 1.09 0 0 1 1.542-1.542l5.721 5.72L22.088.554a1.09 1.09 0 0 1 1.54-.102Z"
       fill="#68D391"
     />
   </svg>
