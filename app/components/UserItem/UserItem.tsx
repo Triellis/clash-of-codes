4981f0bf50dfd501c8b30e15ff0f468680059a5a
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-12-20 16:54:04 +0530

    Update UserItem component with edit mode and new icons

--- app/components/UserItem/UserItem.tsx
+++ app/components/UserItem/UserItem.tsx
@@ -1,8 +1,14 @@
+import Edit from "@/app/styles/Icons/Edit";
+import Tick from "@/app/styles/Icons/Tick";
+import Trash from "@/app/styles/Icons/Trash";
+import { CheckIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/react";
 import React from "react";
 import styles from "./UserItem.module.css";
+import Close from "@/app/styles/Icons/Close";
 
 function UserItem() {
-  let editMode = false;
+  let editMode = true;
 
   if (editMode)
     return (
@@ -19,8 +25,12 @@ function UserItem() {
         <div>Clan</div>
 
         <div className={styles.last}>
-          <div>Save</div>
-          <div>Cancel</div>
+          <div>
+            <IconButton variant={"ghost"} aria-label="Save" icon={<Tick />} />
+          </div>
+          <div>
+            <IconButton variant={"ghost"} aria-label="Save" icon={<Close />} />
+          </div>
         </div>
       </div>
     );
@@ -39,8 +49,12 @@ function UserItem() {
       <div>Clan</div>
 
       <div className={styles.last}>
-        <div>Edit</div>
-        <div>Delete</div>
+        <div>
+          <IconButton variant={"ghost"} aria-label="Edit" icon={<Edit />} />
+        </div>
+        <div>
+          <IconButton variant={"ghost"} aria-label="Delete" icon={<Trash />} />
+        </div>
       </div>
     </div>
   );
