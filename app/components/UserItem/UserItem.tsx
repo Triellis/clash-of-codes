a58fcf1e438a64a727d6c364a7dbc6dc404185d4
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-12-20 16:57:02 +0530

    Add gap between flex items in UserItem module CSS and implement edit mode functionality in UserItem component

--- app/components/UserItem/UserItem.tsx
+++ app/components/UserItem/UserItem.tsx
@@ -1,14 +1,14 @@
+import Close from "@/app/styles/Icons/Close";
 import Edit from "@/app/styles/Icons/Edit";
 import Tick from "@/app/styles/Icons/Tick";
 import Trash from "@/app/styles/Icons/Trash";
 import { CheckIcon } from "@chakra-ui/icons";
 import { IconButton } from "@chakra-ui/react";
-import React from "react";
+import { useState } from "react";
 import styles from "./UserItem.module.css";
-import Close from "@/app/styles/Icons/Close";
 
 function UserItem() {
-  let editMode = true;
+  const [editMode, setEditMode] = useState(false);
 
   if (editMode)
     return (
@@ -29,7 +29,12 @@ function UserItem() {
             <IconButton variant={"ghost"} aria-label="Save" icon={<Tick />} />
           </div>
           <div>
-            <IconButton variant={"ghost"} aria-label="Save" icon={<Close />} />
+            <IconButton
+              variant={"ghost"}
+              aria-label="Save"
+              icon={<Close />}
+              onClick={() => setEditMode(false)}
+            />
           </div>
         </div>
       </div>
@@ -50,7 +55,12 @@ function UserItem() {
 
       <div className={styles.last}>
         <div>
-          <IconButton variant={"ghost"} aria-label="Edit" icon={<Edit />} />
+          <IconButton
+            variant={"ghost"}
+            aria-label="Edit"
+            icon={<Edit />}
+            onClick={() => setEditMode(true)}
+          />
         </div>
         <div>
           <IconButton variant={"ghost"} aria-label="Delete" icon={<Trash />} />
