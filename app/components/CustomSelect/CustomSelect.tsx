241c6bf039baaff1a9bd881f062c325718a9297f
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-12-22 11:33:59 +0530

    Refactor UserAdd component props and state

--- app/components/CustomSelect/CustomSelect.tsx
+++ app/components/CustomSelect/CustomSelect.tsx
@@ -1,34 +1,39 @@
 import { Select } from "@chakra-ui/react";
 
 type Option = {
-  value: string;
-  label: string;
+	value: string | null;
+	label: string;
 };
 
 type CustomSelectProps = {
-  selectOptions: Option[];
-  option: string;
-  setOption: React.Dispatch<React.SetStateAction<string>>;
+	selectOptions: Option[];
+	option: string;
+	setOption: React.Dispatch<React.SetStateAction<string>>;
 };
 
 export default function CustomSelect({
-  selectOptions,
-  option: team,
-  setOption: setTeam,
+	selectOptions,
+	option: team,
+	setOption: setTeam,
 }: CustomSelectProps) {
-  const handleChange = (e: any) => {
-    const selectedOption = e.target.value;
-    setTeam(selectedOption);
-    // console.log(`${teamName} is set to`, selectedTeam);
-  };
+	const handleChange = (e: any) => {
+		const selectedOption = e.target.value;
+		setTeam(selectedOption);
+		// console.log(`${teamName} is set to`, selectedTeam);
+	};
 
-  return (
-    <Select variant="default" size="sm" value={team} onChange={handleChange}>
-      {selectOptions?.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </Select>
-  );
+	return (
+		<Select
+			variant="default"
+			size="sm"
+			value={team}
+			onChange={handleChange}
+		>
+			{selectOptions?.map((option) => (
+				<option key={option.value} value={option.value}>
+					{option.label}
+				</option>
+			))}
+		</Select>
+	);
 }
--- app/components/UserAdd/UserAdd.tsx
+++ app/components/UserAdd/UserAdd.tsx
@@ -7,182 +7,182 @@ import CustomSelect from "../CustomSelect/CustomSelect";
 import styles from "./UserAdd.module.css";
 
 type UserAddProps = {
-  toast: any;
-  isLoading: boolean;
-  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
-  mutate: Function;
-  setPage: React.Dispatch<React.SetStateAction<number>>;
+	toast: any;
+	isLoading: boolean;
+	setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+	mutate: Function;
+	setPage: React.Dispatch<React.SetStateAction<number>>;
 };
 
 type AddUserAction = {
-  field?: "name" | "email" | "cfUsername" | "role" | "clan";
-  value: string;
-  type: "UPDATE" | "RESET";
+	field?: "name" | "email" | "cfUsername" | "role" | "clan";
+	value: string;
+	type: "UPDATE" | "RESET";
 };
 
 function reduceAddUser(
-  state: AddUserState,
-  action: AddUserAction
+	state: AddUserState,
+	action: AddUserAction
 ): AddUserState {
-  switch (action.type) {
-    case "UPDATE":
-      return { ...state, [action.field!]: action.value };
-    case "RESET":
-      return {
-        name: "",
-        email: "",
-        cfUsername: "",
-        role: "Member",
-        clan: "none",
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case "UPDATE":
+			return { ...state, [action.field!]: action.value };
+		case "RESET":
+			return {
+				name: "",
+				email: "",
+				cfUsername: "",
+				role: "Member",
+				clan: "none",
+			};
+		default:
+			return state;
+	}
 }
 
 function UserAdd({
-  toast,
-  isLoading,
-  setIsLoading,
-  mutate,
-  setPage,
+	toast,
+	isLoading,
+	setIsLoading,
+	mutate,
+	setPage,
 }: UserAddProps) {
-  const selectOptions = useMemo(
-    () => [
-      { value: "Admin", label: "Admin" },
-      { value: "Leader", label: "Leader" },
-      { value: "CoLeader", label: "CoLeader" },
-      { value: "Elder", label: "Elder" },
-      { value: "Member", label: "Member" },
-      { value: "Guest", label: "Guest" },
-    ],
-    []
-  );
+	const selectOptions = useMemo(
+		() => [
+			{ value: "Admin", label: "Admin" },
+			{ value: "Leader", label: "Leader" },
+			{ value: "CoLeader", label: "CoLeader" },
+			{ value: "Elder", label: "Elder" },
+			{ value: "Member", label: "Member" },
+			{ value: "Guest", label: "Guest" },
+		],
+		[]
+	);
 
-  const selectClan = useMemo(
-    () => [
-      { value: "none", label: "none" },
-      { value: "BW", label: "Blue Wizards" },
-      { value: "YB", label: "Yellow Barbarians" },
-      { value: "RG", label: "Red Giants" },
-      { value: "PP", label: "Purple Pekkas" },
-    ],
-    []
-  );
+	const selectClan = useMemo(
+		() => [
+			{ value: null, label: "None" },
+			{ value: "BW", label: "Blue Wizards" },
+			{ value: "YB", label: "Yellow Barbarians" },
+			{ value: "RG", label: "Red Giants" },
+			{ value: "PP", label: "Purple Pekkas" },
+		],
+		[]
+	);
 
-  const defaultUser: AddUserState = {
-    name: "",
-    email: "",
-    cfUsername: "",
-    role: "Member",
-    clan: "none",
-  };
+	const defaultUser: AddUserState = {
+		name: "",
+		email: "",
+		cfUsername: "",
+		role: "Member",
+		clan: "none",
+	};
 
-  const [newUser, dispatchUser] = useReducer(reduceAddUser, defaultUser);
+	const [newUser, dispatchUser] = useReducer(reduceAddUser, defaultUser);
 
-  return (
-    <div className={styles.main}>
-      <div>
-        <div>
-          <Input
-            variant={"default"}
-            placeholder="Name"
-            size="sm"
-            value={newUser.name}
-            onChange={(e) =>
-              dispatchUser({
-                type: "UPDATE",
-                field: "name",
-                value: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div>
-          <Input
-            variant={"default"}
-            placeholder="Email"
-            size="sm"
-            value={newUser.email}
-            onChange={(e) =>
-              dispatchUser({
-                type: "UPDATE",
-                field: "email",
-                value: e.target.value,
-              })
-            }
-          />
-        </div>
-      </div>
+	return (
+		<div className={styles.main}>
+			<div>
+				<div>
+					<Input
+						variant={"default"}
+						placeholder="Name"
+						size="sm"
+						value={newUser.name}
+						onChange={(e) =>
+							dispatchUser({
+								type: "UPDATE",
+								field: "name",
+								value: e.target.value,
+							})
+						}
+					/>
+				</div>
+				<div>
+					<Input
+						variant={"default"}
+						placeholder="Email"
+						size="sm"
+						value={newUser.email}
+						onChange={(e) =>
+							dispatchUser({
+								type: "UPDATE",
+								field: "email",
+								value: e.target.value,
+							})
+						}
+					/>
+				</div>
+			</div>
 
-      <div>
-        <Input
-          variant={"default"}
-          placeholder="Username"
-          size="sm"
-          value={newUser.cfUsername}
-          onChange={(e) => {
-            const input = e.target.value;
+			<div>
+				<Input
+					variant={"default"}
+					placeholder="Username"
+					size="sm"
+					value={newUser.cfUsername}
+					onChange={(e) => {
+						const input = e.target.value;
 
-            dispatchUser({
-              field: "cfUsername",
-              value: input,
-              type: "UPDATE",
-            });
-          }}
-        />
-      </div>
+						dispatchUser({
+							field: "cfUsername",
+							value: input,
+							type: "UPDATE",
+						});
+					}}
+				/>
+			</div>
 
-      <div>
-        <CustomSelect
-          selectOptions={selectOptions}
-          option={newUser.role}
-          setOption={(val) => {
-            dispatchUser({
-              type: "UPDATE",
-              field: "role",
-              value: val as string,
-            });
-          }}
-        />
-      </div>
+			<div>
+				<CustomSelect
+					selectOptions={selectOptions}
+					option={newUser.role}
+					setOption={(val) => {
+						dispatchUser({
+							type: "UPDATE",
+							field: "role",
+							value: val as string,
+						});
+					}}
+				/>
+			</div>
 
-      <div>
-        <CustomSelect
-          selectOptions={selectClan}
-          option={newUser.clan}
-          setOption={(val) => {
-            dispatchUser({
-              type: "UPDATE",
-              field: "clan",
-              value: val as string,
-            });
-          }}
-        />
-      </div>
+			<div>
+				<CustomSelect
+					selectOptions={selectClan}
+					option={newUser.clan}
+					setOption={(val) => {
+						dispatchUser({
+							type: "UPDATE",
+							field: "clan",
+							value: val as string,
+						});
+					}}
+				/>
+			</div>
 
-      <div>
-        <div>
-          <IconButton
-            isLoading={isLoading}
-            aria-label="Add"
-            icon={<AddIcon />}
-            width="64px"
-            height="48px"
-            borderRadius="16px"
-            onClick={async () => {
-              setIsLoading(true);
-              setPage(1);
-              await addUser(newUser, mutate, toast);
-              dispatchUser({ type: "RESET", value: "" });
-              mutate();
-              setIsLoading(false);
-            }}
-          />
-        </div>
-      </div>
-    </div>
-  );
+			<div>
+				<div>
+					<IconButton
+						isLoading={isLoading}
+						aria-label="Add"
+						icon={<AddIcon />}
+						width="64px"
+						height="48px"
+						borderRadius="16px"
+						onClick={async () => {
+							setIsLoading(true);
+							setPage(1);
+							await addUser(newUser, mutate, toast);
+							dispatchUser({ type: "RESET", value: "" });
+							mutate();
+							setIsLoading(false);
+						}}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default UserAdd;
