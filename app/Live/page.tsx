91c6f9dee6ba4d69b8245c4d7c6690d06a9dd71a
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-12-25 17:31:02 +0530

    Refactor leaderboard tabs initialization and update logic

--- app/Live/page.tsx
+++ app/Live/page.tsx
@@ -10,15 +10,13 @@ import { fullForm } from "../util/functions";
 const WebSocketComponent = () => {
 	const [leaderboardArr, setLeaderboardArr] = useState<LiveLeaderboard>([]);
 	const [tabIndex, setTabIndex] = useState(0);
-	const tabs = leaderboardArr.map((item, idx) => {
-		const teams = Object.keys(item);
-		const team1 = teams[0];
-		const team2 = teams[1];
-		const team1Full = fullForm(team1).split(" ")[1];
-		const team2Full = fullForm(team2).split(" ")[1];
-		const joint = `${team1Full} vs ${team2Full}`;
-		return { label: joint, value: joint, index: idx };
-	});
+	let [tabs, setTabs] = useState([
+		{
+			value: "Loading...",
+			label: "Loading...",
+			index: 0,
+		},
+	]);
 
 	const [tab, setTab] = useState<string>("");
 
@@ -30,6 +28,18 @@ const WebSocketComponent = () => {
 			}
 		});
 	}, [tab]);
+	useEffect(() => {
+		const newTabs = leaderboardArr.map((item, idx) => {
+			const teams = Object.keys(item);
+			const team1 = teams[0];
+			const team2 = teams[1];
+			const team1Full = fullForm(team1).split(" ")[1];
+			const team2Full = fullForm(team2).split(" ")[1];
+			const joint = `${team1Full} vs ${team2Full}`;
+			return { label: joint, value: joint, index: idx };
+		});
+		setTabs(newTabs);
+	}, [leaderboardArr]);
 	useEffect(() => {
 		// Establish WebSocket connection
 		const ws = new WebSocket("ws://localhost:3001");
