5a958b5bef61386649f753120378f8520e37acf5
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-12-30 16:33:48 +0530

    Add responsive behavior to Live page

--- app/Live/page.tsx
+++ app/Live/page.tsx
@@ -6,7 +6,34 @@ import Live from "../styles/Icons/Live";
 import { fullForm } from "../util/functions";
 import { LiveLeaderboard, TabsType } from "../util/types";
 import styles from "./Live.module.css";
-
+function useWindowSizeMobile() {
+	// Initialize state with undefined width/height so server and client renders match
+	// Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+	const [windowSize, setWindowSize] = useState<{
+		width: number | undefined;
+		height: number | undefined;
+	}>({
+		width: undefined,
+		height: undefined,
+	});
+	useEffect(() => {
+		// Handler to call on window resize
+		function handleResize() {
+			// Set window width/height to state
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+		}
+		// Add event listener
+		window.addEventListener("resize", handleResize);
+		// Call handler right away so state gets updated with initial window size
+		handleResize();
+		// Remove event listener on cleanup
+		return () => window.removeEventListener("resize", handleResize);
+	}, []); // Empty array ensures that effect is only run on mount
+	return windowSize.width ? windowSize.width < 768 : true;
+}
 const WebSocketComponent = () => {
 	const [leaderboardArr, setLeaderboardArr] = useState<LiveLeaderboard>([]);
 
@@ -30,6 +57,7 @@ const WebSocketComponent = () => {
 	]);
 
 	const [tab, setTab] = useState<string>("");
+	const isSmall = useWindowSizeMobile();
 
 	useEffect(() => {
 		tabs.forEach((t) => {
@@ -87,16 +115,20 @@ const WebSocketComponent = () => {
 				<Live />
 				Live Score Board
 			</div>
-
-			<div className={styles.tabs}>
+			{!isSmall && (
 				<TabsComponent
 					tab={tab} // Pass the 'tab' state variable
 					setTab={setTab}
 					allTabs={tabs}
 				/>
-			</div>
-
-			<Leaderboard fetchedData={leaderboardArr[tabIndex]} />
+			)}
+			{isSmall ? (
+				leaderboardArr.map((item, idx) => (
+					<Leaderboard fetchedData={item} key={idx} />
+				))
+			) : (
+				<Leaderboard fetchedData={leaderboardArr[tabIndex]} />
+			)}
 		</div>
 	);
 };
