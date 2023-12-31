964426d2b246e25c27a4ccc1fd5d85ffbfc16a21
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-12-31 20:35:23 +0530

    Add Counter component and update Test page

--- app/Test/page.tsx
+++ app/Test/page.tsx
@@ -1,37 +1,19 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../components/Logo/Logo";
 import NavBar from "../components/NavBar/NavBar";
 import NavItem from "../components/NavItem/NavItem";
 import styles from "./page.module.css";
 import { Flex } from "@chakra-ui/react";
 import { customFetch, getServerUrl, getUserData } from "../util/functions";
+import Counter from "../components/Counter";
 
 export default function Test() {
-	const [isOpen, setIsOpen] = React.useState(false);
+	const [number, setNumber] = useState(1);
 	return (
 		<main className={styles.main}>
-			<button
-				onClick={async () => {
-					const dummyContest = {
-						Team1: "BW",
-						Team2: "YB",
-						ContestCode: "12345",
-						Live: true,
-					};
-
-					const res = await customFetch(`admin/config`, {
-						method: "POST",
-						headers: {
-							"Content-Type": "application/json",
-						},
-						body: JSON.stringify(dummyContest),
-					});
-				}}
-			>
-				Click
-			</button>
+			<Counter setNumber={setNumber} startNumber={number} />
 		</main>
 	);
 }
---
+++ app/components/Counter.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useRef, useCallback, useMemo } from "react";
+import PropTypes from "prop-types";
+
+Counter.propTypes = {
+	startNumber: PropTypes.number.isRequired,
+	setNumber: PropTypes.func.isRequired,
+};
+
+function Counter({
+	startNumber,
+	setNumber,
+}: {
+	startNumber: number;
+	setNumber: any;
+}) {
+	const flipCardRef1 = useRef<HTMLDivElement>(null);
+	const flipCardRef2 = useRef<HTMLDivElement>(null);
+
+	const updateFlipCard = useCallback(
+		(flipCardRef: any, currentValue: number, nextValue: number) => {
+			const flipCard = flipCardRef.current;
+			const topHalf = flipCard.querySelector(".top");
+			const bottomHalf = flipCard.querySelector(".bottom");
+			const topFlip = document.createElement("div");
+			const bottomFlip = document.createElement("div");
+
+			topFlip.classList.add("top-flip");
+			bottomFlip.classList.add("bottom-flip");
+
+			bottomHalf.textContent = currentValue;
+			topFlip.textContent = String(currentValue);
+			bottomFlip.textContent = String(nextValue);
+
+			topFlip.addEventListener("animationstart", () => {
+				topHalf.textContent = nextValue;
+			});
+
+			topFlip.addEventListener("animationend", () => {
+				topFlip.remove();
+			});
+
+			bottomFlip.addEventListener("animationend", () => {
+				bottomHalf.textContent = nextValue;
+				bottomFlip.remove();
+			});
+
+			flipCard.append(topFlip, bottomFlip);
+		},
+		[]
+	);
+
+	const number = useMemo(() => startNumber % 10, [startNumber]);
+
+	useEffect(() => {
+		if (flipCardRef2.current) {
+			updateFlipCard(flipCardRef1, number, (number + 1) % 10);
+
+			if (Math.floor(startNumber / 9) === 0 || startNumber <= 0) {
+				flipCardRef2.current.style.display = "none";
+			} else {
+				flipCardRef2.current.style.display = "inline-flex";
+				updateFlipCard(
+					flipCardRef2,
+					Math.floor(startNumber / 10),
+					Math.floor((startNumber + 1) / 10)
+				);
+			}
+		}
+	}, [startNumber, updateFlipCard, number]);
+
+	const handleClick = () => {
+		setNumber((prevNumber: number) => (prevNumber + 1) % 100);
+	};
+
+	return (
+		<>
+			<div className="flip-card" ref={flipCardRef2}>
+				<div className="top"></div>
+				<div className="bottom"></div>
+			</div>
+			<div className="flip-card" ref={flipCardRef1}>
+				<div className="top"></div>
+				<div className="bottom"></div>
+			</div>
+			<button onClick={handleClick}>increase</button>
+		</>
+	);
+}
+
+export default Counter;
