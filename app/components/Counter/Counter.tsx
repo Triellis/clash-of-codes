a367a73ab8db6d00a7831fc60cc261fb018e79de
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-01-02 13:08:14 +0530

    Refactor Counter component to improve readability and maintainability

--- app/components/Counter/Counter.tsx
+++ app/components/Counter/Counter.tsx
@@ -1,3 +1,4 @@
+import { update } from "@/app/util/userSlice";
 import PropTypes from "prop-types";
 import { useCallback, useEffect, useMemo, useRef } from "react";
 import styles from "./Counter.module.css";
@@ -16,30 +17,43 @@ function Counter({
 }) {
 	const flipCardRef1 = useRef<HTMLDivElement>(null);
 	const flipCardRef2 = useRef<HTMLDivElement>(null);
-	const topRef = useRef<HTMLDivElement>(null);
-	const bottomRef = useRef<HTMLDivElement>(null);
+	const topRef1 = useRef<HTMLDivElement>(null);
+	const bottomRef1 = useRef<HTMLDivElement>(null);
+	const topRef2 = useRef<HTMLDivElement>(null);
+	const bottomRef2 = useRef<HTMLDivElement>(null);
 
 	const updateFlipCard = useCallback(
 		(flipCardRef: any, currentValue: number, nextValue: number) => {
 			const flipCard = flipCardRef.current;
-			const topHalf = topRef.current;
-			const bottomHalf = bottomRef.current;
+			const topHalf1 = topRef1.current;
+			const bottomHalf1 = bottomRef1.current;
+			const topHalf2 = topRef2.current;
+			const bottomHalf2 = bottomRef2.current;
 			const topFlip = document.createElement("div");
 			const bottomFlip = document.createElement("div");
-			console.log(topHalf, bottomHalf, styles.top, styles.bottom);
 
-			if (topHalf && bottomHalf && topFlip && bottomFlip) {
+			console.log("vals", currentValue, nextValue);
+
+			if (
+				topHalf2 &&
+				bottomHalf2 &&
+				topHalf1 &&
+				bottomHalf1 &&
+				topFlip &&
+				bottomFlip
+			) {
 				topFlip.classList.add(styles.topFlip);
 				bottomFlip.classList.add(styles.bottomFlip);
-				// print top and bottom halves
-				console.log("top", topHalf);
-				console.log("bottom", bottomHalf);
 
-				bottomHalf.textContent = String(currentValue);
+				bottomHalf1.textContent = String(currentValue);
+
 				topFlip.textContent = String(currentValue);
 				bottomFlip.textContent = String(nextValue);
+
 				topFlip.addEventListener("animationstart", () => {
-					topHalf.textContent = String(nextValue);
+					console.log("next top", nextValue);
+					topHalf1.textContent = String(nextValue);
+					topHalf2.textContent = String(currentValue);
 				});
 
 				topFlip.addEventListener("animationend", () => {
@@ -47,7 +61,9 @@ function Counter({
 				});
 
 				bottomFlip.addEventListener("animationend", () => {
-					bottomHalf.textContent = String(nextValue);
+					console.log("next bottom", nextValue);
+					bottomHalf1.textContent = String(nextValue);
+					bottomHalf2.textContent = String(currentValue);
 					bottomFlip.remove();
 				});
 
@@ -82,13 +98,15 @@ function Counter({
 
 	return (
 		<div className={styles.main}>
+			{/* tens digit */}
 			<div className={styles.flipCard} ref={flipCardRef2}>
-				<div className={styles.top} ref={topRef}></div>
-				<div className={styles.bottom} ref={bottomRef}></div>
+				<div className={styles.top} ref={topRef2}></div>
+				<div className={styles.bottom} ref={bottomRef2}></div>
 			</div>
+			{/* unit digit */}
 			<div className={styles.flipCard} ref={flipCardRef1}>
-				<div className={styles.top} ref={topRef}></div>
-				<div className={styles.bottom} ref={bottomRef}></div>
+				<div className={styles.top} ref={topRef1}></div>
+				<div className={styles.bottom} ref={bottomRef1}></div>
 			</div>
 
 			<button onClick={handleClick}>increase</button>
