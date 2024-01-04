0b9109448b3fb4e6bdbb79cf9f4dd81b828882fe
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-01-04 12:03:24 +0530

    Add Footer component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,37 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { useState } from "react";
+import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import { Providers } from "./providers";
 import "./styles/globals.css";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Clash Of Codes",
-  description: "The Live Coding Competition",
+	title: "Clash Of Codes",
+	description: "The Live Coding Competition",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  // const [isOpen, setIsOpen] = useState(false);
+	// const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <html lang="en">
-      <body>
-        <Providers>
-          <div className="nav">
-            <NavBar />
-          </div>
-          <div id="root">{children}</div>
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<Providers>
+					<div className="nav">
+						<NavBar />
+					</div>
+					<div id="root">{children}</div>
+					<div>
+						<Footer />
+					</div>
+				</Providers>
+			</body>
+		</html>
+	);
 }
