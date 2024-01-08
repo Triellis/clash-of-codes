8b5ea6329fa9aeaf10f527fb08be6b9a24ac8915
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-01-08 19:16:19 +0530

    Add Google Analytics tracking to Providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,10 +6,11 @@ import { theme } from "./styles/theme";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./util/store";
-
+import { GoogleAnalytics } from "nextjs-google-analytics";
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<ReduxProvider store={store}>
+			<GoogleAnalytics trackPageViews />
 			<GoogleOAuthProvider clientId="910407207351-rujuf61sg8t5jk5vco6cll51474fgm5v.apps.googleusercontent.com">
 				<ChakraProvider theme={theme}>{children}</ChakraProvider>
 			</GoogleOAuthProvider>
