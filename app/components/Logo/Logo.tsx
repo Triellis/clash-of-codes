f7f105479e3c36d7512882120f685b02e13e476c
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-01-13 20:49:33 +0530

    Update Leaderboard and Logo components also resolves #47

--- app/components/Logo/Logo.tsx
+++ app/components/Logo/Logo.tsx
@@ -1,20 +1,23 @@
 import { Heading } from "@chakra-ui/react";
+import Link from "next/link";
 import styles from "./Logo.module.css";
 export default function Logo() {
 	return (
-		<Heading
-			fontSize={{
-				sm: 16,
-				md: 24,
-			}}
-			textAlign={"center"}
-			height="100%"
-			justifyContent={"center"}
-			alignItems={"center"}
-			display={"flex"}
-			className="logo"
-		>
-			Clash OF Codes
-		</Heading>
+		<Link href={"/Live"}>
+			<Heading
+				fontSize={{
+					sm: 16,
+					md: 24,
+				}}
+				textAlign={"center"}
+				height="100%"
+				justifyContent={"center"}
+				alignItems={"center"}
+				display={"flex"}
+				className="logo"
+			>
+				Clash OF Codes
+			</Heading>
+		</Link>
 	);
 }
