2d4123f736d8ac0783be8c59a566f3d4b5702a20
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-01-23 16:09:39 +0530

    Update page layout and timer functionality

--- app/Test/page.tsx
+++ app/Test/page.tsx
@@ -1,25 +1,69 @@
 "use client";
-
-import { Button, Flex, Input } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { Button } from "@chakra-ui/react";
+import React, { use, useEffect, useState } from "react";
 import Counter from "../components/Counter/Counter";
-
 import styles from "./page.module.css";
 
+function timeDec({
+	mins,
+	hrs,
+	sec,
+	setMin,
+	setHrs,
+	setSec,
+}: {
+	mins: number;
+	hrs: number;
+	sec: number;
+	setMin: any;
+	setHrs: any;
+	setSec: any;
+}) {
+	if (sec === 0) {
+		if (mins === 0) {
+			if (hrs === 0) {
+				// Timer is already at zero, do nothing
+				return;
+			}
+
+			// Decrement hours and set minutes to 59
+			hrs--;
+			setHrs(hrs);
+			setMin(59);
+		} else {
+			// Decrement minutes and set seconds to 59
+			mins--;
+			setMin(mins);
+			setSec(59);
+		}
+	} else {
+		// Decrement seconds
+		sec--;
+		setSec(sec);
+	}
+}
+
 export default function Test() {
-	const [number, setNumber] = useState(1);
+	const [hrs, setHrs] = useState(1);
+	const [mins, setMins] = useState(30);
+	const [sec, setSec] = useState(0);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			timeDec({ mins, hrs, sec, setMin: setMins, setHrs, setSec });
+		}, 1000);
+		return () => clearInterval(timer);
+	}, [mins, hrs, sec]);
 
 	return (
 		<main className={styles.main}>
-			<Counter startNumber={number} />
+			<Counter startNumber={hrs} />
+			{":"}
+			<Counter startNumber={mins} />
+			{":"}
+			<Counter startNumber={sec} />
 
-			<Button
-				onClick={(e) => {
-					setNumber(number + 1);
-				}}
-			>
-				+
-			</Button>
+			<Button>+</Button>
 		</main>
 	);
 }
