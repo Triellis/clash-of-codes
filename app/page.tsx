b545c597792849f187916b3b766d7fe8183f2d95
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-01-23 17:44:46 +0530

    Redirect to "/Live" page in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,7 @@
 
 import { useEffect } from "react";
 
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 export default function Home() {
-	const router = useRouter();
-	useEffect(() => {
-		router.push("/Live");
-	}, []);
-	return <main>Home Page</main>;
+	redirect("/Live");
 }
